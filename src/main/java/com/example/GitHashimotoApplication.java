799b5c714c77a71eb124fc90e5c9f54084b1f7f3
hashimotokairi <kairi.hashimoto@rakus.co.jp>
2021-06-04 12:18:44 +0900

    firstCommit

---
+++ src/main/java/com/example/GitHashimotoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHashimotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHashimotoApplication.class, args);
+	}
+
+}
